2393de0f7323056afefe3fa6ce3be8738e107ef3
xyt417 <417639959@qq.com>
2023-04-28 13:13:02 +0800

    MBR output though VMEMORY

--- src/mbr.asm
+++ src/mbr.asm
@@ -8,6 +8,10 @@ section MBR vstart=0x7c00
     mov fs, ax
     mov sp, 0x7c00
 
+; 显存位置
+    mov ax, 0xb800
+    mov gs, ax
+
 ; 清屏
 ; AH: 功能号 0x06
 ; AL: 上卷行数 (0表示全部)
@@ -20,20 +24,39 @@ section MBR vstart=0x7c00
     mov dx, 0x184f  ; (79, 24)
     int 0x10
 
-; 获取光标位置
-; 功能号0x03： 获取光标位置
-    mov ah, 0x03
-    mov bh, 0 ; 默认页号
-    int 0x10
-; 输出: dh = 光标所在行， dl = 光标所在列
+; ; 获取光标位置
+; ; 功能号0x03： 获取光标位置
+;     mov ah, 0x03
+;     mov bh, 0 ; 默认页号
+;     int 0x10
+; ; 输出: dh = 光标所在行， dl = 光标所在列
     
-; 打印字符串
-    mov ax, message
-    mov bp, ax
-    mov cx, 10
-    mov ax, 0x1301
-    mov bx, 0x3 ; 绿色 bh: 页号  bl: 字符颜色
-    int 0x10
+; ; 打印字符串
+;     mov ax, message
+;     mov bp, ax
+;     mov cx, 10
+;     mov ax, 0x1301
+;     mov bx, 0x3 ; 绿色 bh: 页号  bl: 字符颜色
+;     int 0x10
+
+; 直接向显存打印字符
+    mov byte [gs:0], 'H'
+    mov byte [gs:1], 0xA4
+    mov byte [gs:2], 'e'
+    mov byte [gs:3], 0xA4
+    mov byte [gs:4], 'l'
+    mov byte [gs:5], 0xA4
+    mov byte [gs:6], 'l'
+    mov byte [gs:7], 0xA4
+    mov byte [gs:8], 'o'
+    mov byte [gs:9], 0xA4
+    mov byte [gs:10], ' '
+    mov byte [gs:12], 'M'
+    mov byte [gs:13], 0xC1
+    mov byte [gs:14], 'B'
+    mov byte [gs:15], 0xC1
+    mov byte [gs:16], 'R'
+    mov byte [gs:17], 0xC1
 
 
 ; 悬挂
