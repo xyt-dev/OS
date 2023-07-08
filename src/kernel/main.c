c8f59e949a89a9a9d8ba402bd3c2539cb2f248d3
WTFSC <72695575+xyt417@users.noreply.github.com>
2023-07-08 17:24:02 +0800

    hello kernel
    qemu歇歇

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,10 @@
 int main(){
+    char *str = "Hello Kernel!";
+    char *screen = (char *)0xb8160;
+    for(int i = 0; i < 13; i++){
+        screen[i*2] = str[i];
+        screen[i*2+1] = 0x07;
+    }
     while(1);
     return 0;
 }
